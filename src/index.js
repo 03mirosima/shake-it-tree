79f5a028e6a8de7ccdb55e3e8f4193d4bf8b0cc8
03mirosima <sezenmiray2009@hotmail.com>
2021-10-11 15:25:19 +0300

    Added redux and created store

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
 import { render } from "react-dom";
+import store from "./store/store";
+import { Provider } from "react-redux";
 import App from "./components/App";
-
-render(<App />, document.getElementById("root"));
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
