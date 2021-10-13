cdcac09ec4adceeb78a8e7f28f0e7b84b1d9ed33
03mirosima <sezenmiray2009@hotmail.com>
2021-10-13 10:10:48 +0300

    Added control to see if tree has any apples

--- src/components/Tree.jsx
+++ src/components/Tree.jsx
@@ -8,21 +8,28 @@ import Basket from "./Basket";
 const Tree = () => {
   const apples = useSelector(selectAllApples);
   const isShaking = useSelector(({ tree }) => tree.treeShake);
+  const dropControl = apples.filter((apple) => {
+    return apple.isDropped === true;
+  });
   return (
     <>
       <ShakeButton />
       <div className="tree-wrapper">
         <img className={` ${isShaking && "tree-shaking"}`} src={tree} />
-        {/* We map the apples for showing them on the tree */}
-        {apples.map((a) => (
-          <Apples
-            key={a.id}
-            style={a}
-            className={` ${
-              isShaking && a.isDropped === false ? "apple-shaking" : ""
-            }`}
-          />
-        ))}
+        {/* We map the apples for showing them on the tree then control if tree has any apples*/}
+        {dropControl.length === 9 ? (
+          <h5>Üzgünüm daha elma kalmadı :(</h5>
+        ) : (
+          apples.map((a) => (
+            <Apples
+              key={a.id}
+              style={a}
+              className={` ${
+                isShaking && a.isDropped === false ? "apple-shaking" : ""
+              }`}
+            />
+          ))
+        )}
       </div>
       <Basket />
     </>
